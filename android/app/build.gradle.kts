36f24a0d483b806dc97aad6d673036a3e13125fa
auddrriiicc <062340722950@student.polsri.ac.id>
2026-09-25 13:28:34 +0700

    PLSSS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.bank_sampah_mobile"
-    
-    // PERBAIKAN: Diubah langsung ke angka 36 untuk memenuhi syarat dependensi
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -17,24 +14,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.bank_sampah_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        
-        // PERBAIKAN: Diubah langsung ke angka 36
         targetSdk = 36
-        
-        // Uses the version code from pubspec.yaml.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,21 +30,10 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
 flutter {
     source = "../.."
-}
-
-// PERBAIKAN: Memaksa semua sub-plugin (seperti file_picker & flutter_plugin_android_lifecycle) menggunakan compileSdk 36
-subprojects {
-    afterEvaluate { project ->
-        if (project.hasProperty('android')) {
-            project.android {
-                compileSdkVersion 36
-            }
-        }
-    }
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,20 +5,32 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// PERBAIKAN: Memaksa SDK 36 langsung pada tahap konfigurasi proyek (bebas dari error afterEvaluate)
+subprojects {
+    plugins.withId("com.android.application") {
+        configure<com.android.build.gradle.BaseExtension> {
+            compileSdkVersion(36)
+        }
+    }
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.BaseExtension> {
+            compileSdkVersion(36)
+        }
+    }
 }
